09529109b9cbe5d4bc640f2befc6b38d0b93d980
entranco <afraser276@gmail.com>
2019-10-31 17:10:57 -0600

    added starter files for frameManager and animationPreview

--- framemanager.h
+++ framemanager.h
@@ -2,6 +2,7 @@
 #define FRAMEMANAGER_H
 
 #include <QWidget>
+#include <QImage>
 
 namespace Ui {
 class FrameManager;
@@ -15,6 +16,21 @@ public:
     explicit FrameManager(QWidget *parent = nullptr);
     ~FrameManager();
     Ui::FrameManager *ui;
+
+    std::vector<QImage*> getFrames();
+    void setFrames(std::vector<QImage*>);
+    void setSize(int);
+    void reloadFrame();
+
+signals:
+    void changeCurrFrame(QImage*);
+    void changeFrameStructure(std::vector<QImage*>);
+
+
+private:
+    std::vector<QImage*> frames;
+    int currFrame;
+
 };
 
 #endif // FRAMEMANAGER_H
